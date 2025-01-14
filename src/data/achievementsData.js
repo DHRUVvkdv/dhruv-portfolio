ee6cb95ade7a1ef4a3283978bf5ace7b4f34b772
Dhruv Varshney <34621585+DHRUVvkdv@users.noreply.github.com>
2025-01-13 23:55:07 -0500

    update: added more stuff to achievements page

--- src/data/achievementsData.js
+++ src/data/achievementsData.js
@@ -3,6 +3,20 @@
 export const achievementsData = [
     {
         id: 1,
+        title: "📝 Outstanding Participation CS 3114 - Data Structures and Algorithms",
+        category: "certificate",
+        date: "December 2024",
+        description: "Recieved the award for outstanding participation in the course CS 3114 - Data Structures and Algorithms by instructor Sally Hamouda.",
+        images: [
+            " https://res.cloudinary.com/dru4ekf2q/image/upload/v1736813968/Portfolio/achievements/IMG_6233_xqjr5n.jpg",
+        ],
+        impact: [
+            "Actively participated in class discussions",
+            "Asked insightful questions",
+        ],
+    },
+    {
+        id: 2,
         title: "🥇 First Place - Marriott Codefest",
         category: "hackathon",
         date: "October 2024",
@@ -25,64 +39,167 @@ export const achievementsData = [
         }
     },
     {
-        id: 2,
-        title: "Research Presentation - AI Conference",
-        category: "research",
-        date: "January 2024",
-        description: "Presented research findings on 'Machine Learning Applications in Healthcare' at the International AI Conference.",
-        images: [
-            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1711283880/wgpnkcaeffwagtetpfkd.jpg",
-            "https://res.cloudinary.com/dru4ekf2q/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1711284211/hntg7xcxdyuj3w9w1pmk.jpg"
-        ],
-        impact: [
-            "Selected among top 10 presentations",
-            "Published in conference proceedings",
-            "Collaboration opportunities with 2 universities"
-        ],
-        links: {
-            paper: "url-to-paper",
-            presentation: "url-to-presentation"
-        }
-    },
-    {
         id: 3,
-        title: "AWS Certified Solutions Architect",
-        category: "certificate",
-        date: "December 2023",
-        description: "Achieved AWS Solutions Architect Professional certification, demonstrating expertise in designing distributed systems.",
+        title: "🏆 Winner - CoStar Group - Best Real Estate Hack (VTHacks 12) - VT",
+        category: "hackathon",
+        date: "September 2024",
+        description: "Created a web app to allow users to review and rate buildings based on their accessibility features, such as ramps, elevators, and various other amenities.",
         images: [
-            "https://res.cloudinary.com/dru4ekf2q/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1711284211/hntg7xcxdyuj3w9w1pmk.jpg",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTABbXr4i-QODqhy7tofHYmTYh05rYPktzacw&s"
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736814838/Portfolio/achievements/_DSC0730_2_srslna.jpg",
         ],
         impact: [
-            "Scored in the top 95 percentile",
-            "Completed 5 real-world projects",
-            "Led cloud architecture workshop for juniors"
+            "Won in a competition of 650+ participants",
+            "Used cutting-edge technologies such as AWS, React, Node.js, MongoDB",
+            "Project deployed to production"
         ],
         links: {
-            certificate: "url-to-certificate",
-            badge: "url-to-linkedin-badge"
+            website: "https://mapability.dhruvv.dev/",
+            code: "https://github.com/DHRUVvkdv/mapability-backend",
+            devpost: "https://devpost.com/software/mapability-16m47i"
         }
     },
     {
         id: 4,
-        title: "Dean's Innovation Award",
-        category: "award",
-        date: "November 2023",
-        description: "Received the Dean's Innovation Award for developing a novel approach to campus sustainability tracking.",
+        title: "Presented at 2024 Virginia Tech Undergraduate Research Conference",
+        category: "research",
+        date: "July 2024",
+        description: "Presented 2 projects at the 2024 Virginia Tech Undergraduate Research Conference, showcasing the use if Artificial Intelliegence in education environmental lab enhancements.",
         images: [
-            "https://res.cloudinary.com/dru4ekf2q/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1711284211/hntg7xcxdyuj3w9w1pmk.jpg",
-            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1711283880/wgpnkcaeffwagtetpfkd.jpg"
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736815799/Portfolio/achievements/summerresearch/IMG_3529_xu5kcf.jpg",
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736815816/Portfolio/achievements/summerresearch/IMG_4037_giqjhc.jpg",
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736815800/Portfolio/achievements/summerresearch/IMG_4031_ovtohn.jpg",
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736815801/Portfolio/achievements/summerresearch/IMG_4032_oa8ofg.jpg"
         ],
         impact: [
-            "Implemented across 3 university campuses",
-            "Reduced energy consumption by 25%",
-            "Featured in local tech magazine"
+            "Worked on 2 research projects throughout the summer",
+            "Succesfully demonstrated the use of AI in education and environmental lab",
+            "Received positive feedback from professors and peers"
         ],
         links: {
-            article: "url-to-article",
-            project: "url-to-project"
+            conference: "https://hdl.handle.net/10919/120919",
+            presentation1: "https://drive.google.com/file/d/1348zlvlnFLY7HtT24NqjPSKO8-OPQawr/view?usp=drive_link",
+            presentation2: "https://drive.google.com/file/d/1bl0gw2pvssemah-edSWeZe1zMpbPprsS/view?usp=drive_link"
         }
+    },
+    {
+        id: 5,
+        title: "Dean's List with Distinction - Virginia Tech",
+        category: "award",
+        date: "May 2024",
+        description: "Received the Dean's List with Distinction award for outstanding achievement in academics at Virginia Tech for the Spring 2024 semester.",
+        images: [
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736818195/Portfolio/achievements/vtlogo_vlqrv4.png",
+        ],
+    },
+    {
+        id: 6,
+        title: "🥇 First Place - Accessibility & Empowerment (HooHacks 2024) - UVA",
+        category: "hackathon",
+        date: "March 2024",
+        description: "Received first place in the Accessibility & Empowerment category at HooHacks 2024 for creating a web and mobile app that tranforms how news is consumed.",
+        images: [
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736817200/Portfolio/achievements/hoohacks2024/IMG_8500_gwh5z4.jpg",
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736817252/Portfolio/achievements/hoohacks2024/IMG_8514_3_twle8i.jpg",
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736817199/Portfolio/achievements/hoohacks2024/IMG_8479_vskd9c.jpg"
+        ],
+        impact: [
+            "Implemented a web and mobile app to transform news consumption",
+            "Made it accesible for people with Dyslexia",
+            "Won in a competition of 550+ participants"
+        ],
+        links: {
+            code: "https://github.com/DHRUVvkdv/hoohacks2024administrator",
+            devpost: "https://devpost.com/software/bytes-xh4yrk"
+        }
+    },
+    {
+        id: 7,
+        title: "🏆 Winner - Peraton - Best Webhack using React (HackViolet 12) - VT",
+        category: "hackathon",
+        date: "February 2024",
+        description: "Received first place in the best webhack using React category at HackViolet 2024 by Peraton for creating a web app that helps women to have proper diet and exercise during different menstrual phases.",
+        images: [
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736819669/Portfolio/achievements/hackviolet2024/DSC_0317-Enhanced-NR_zbqpaw.jpg",
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736819703/Portfolio/achievements/hackviolet2024/DSC_0634-Enhanced-NR_c3af8w.jpg",
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736819744/Portfolio/achievements/hackviolet2024/hackvioletcertificate_go6yqi.jpg"
+        ],
+        impact: [
+            "Implemented a web app to help women with diet and exercise during menstrual phases",
+            "Coded the project in 24 hours",
+            "Deployed the project to production"
+        ],
+        links: {
+            video: "https://www.youtube.com/watch?v=lIWEmkGtWFI",
+            website: "https://rhythmandrise.dhruvv.dev/",
+            code: "https://github.com/DHRUVvkdv/Hack-Violet-2024",
+            devpost: "https://devpost.com/software/not-decided-yet-8fk4er",
+        }
+    },
+    {
+        id: 8,
+        title: "Dean's List with Distinction - Virginia Tech",
+        category: "award",
+        date: "Dec 2023",
+        description: "Received the Dean's List with Distinction award for outstanding achievement in academics at Virginia Tech for the Fall 2023 semester.",
+        images: [
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736818195/Portfolio/achievements/vtlogo_vlqrv4.png",
+        ],
+    },
+    {
+        id: 9,
+        title: "🏆 Winner - Capital One: Best Financial Hack (VTHacks 11) - VT",
+        category: "hackathon",
+        date: "September 2023",
+        description: "Received first place in the best webhack using React category at HackViolet 2024 by Peraton for creating a web app that helps women to have proper diet and exercise during different menstrual phases.",
+        images: [
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736819669/Portfolio/achievements/hackviolet2024/DSC_0317-Enhanced-NR_zbqpaw.jpg",
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736819703/Portfolio/achievements/hackviolet2024/DSC_0634-Enhanced-NR_c3af8w.jpg",
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736819744/Portfolio/achievements/hackviolet2024/hackvioletcertificate_go6yqi.jpg"
+        ],
+        impact: [
+            "Implemented a web app to help women with diet and exercise during menstrual phases",
+            "Coded the project in 24 hours",
+            "Deployed the project to production"
+        ],
+        links: {
+            video: "https://www.youtube.com/watch?v=lIWEmkGtWFI",
+            website: "https://rhythmandrise.dhruvv.dev/",
+            code: "https://github.com/DHRUVvkdv/Hack-Violet-2024",
+            devpost: "https://devpost.com/software/stonks-simulator-7hvpxe",
+        }
+    },
+    {
+        id: 10,
+        title: "🏆 Winner - MLH: Auth0 - Best Use of Auth0 (VTHacks 11) - VT",
+        category: "hackathon",
+        date: "September 2023",
+        description: "Received first place in the best webhack using React category at HackViolet 2024 by Peraton for creating a web app that helps women to have proper diet and exercise during different menstrual phases.",
+        images: [
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736819669/Portfolio/achievements/hackviolet2024/DSC_0317-Enhanced-NR_zbqpaw.jpg",
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736819703/Portfolio/achievements/hackviolet2024/DSC_0634-Enhanced-NR_c3af8w.jpg",
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736819744/Portfolio/achievements/hackviolet2024/hackvioletcertificate_go6yqi.jpg"
+        ],
+        impact: [
+            "Implemented a web app to help women with diet and exercise during menstrual phases",
+            "Coded the project in 24 hours",
+            "Deployed the project to production"
+        ],
+        links: {
+            video: "https://www.youtube.com/watch?v=lIWEmkGtWFI",
+            website: "https://rhythmandrise.dhruvv.dev/",
+            code: "https://github.com/DHRUVvkdv/Hack-Violet-2024",
+            devpost: "https://devpost.com/software/stonks-simulator-7hvpxe",
+        }
+    },
+    {
+        id: 11,
+        title: "President's List - Virginia Tech",
+        category: "award",
+        date: "Dec 2022",
+        description: "Received the President's List award for outstanding achievement in academics at Virginia Tech for the Fall 2022 semester.",
+        images: [
+            "https://res.cloudinary.com/dru4ekf2q/image/upload/v1736818195/Portfolio/achievements/vtlogo_vlqrv4.png",
+        ],
     }
 ];
 
